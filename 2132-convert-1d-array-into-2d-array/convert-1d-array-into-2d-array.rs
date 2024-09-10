057897c846d8b8c5818073a59dbdacfe2512bfa1
Hruday <150934405+hruday-HMS69@users.noreply.github.com>
2024-09-11 00:30:09 +0530

    Time: 32 ms (25.36%) | Memory: 4.4 MB (82.22%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.rs
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.rs
@@ -1,14 +1,17 @@
 impl Solution {
     pub fn construct2_d_array(original: Vec<i32>, m: i32, n: i32) -> Vec<Vec<i32>> {
-        if original.len() != m as usize * n as usize {
+        let total_elements = m * n;
+
+        
+        if original.len() as i32 != total_elements {
             return vec![];
         }
 
+        
         let mut ans = vec![vec![0; n as usize]; m as usize];
-        let mut idx = 0;
-        for a in original {
-            ans[idx / n as usize][idx % n as usize] = a;
-            idx += 1;
+
+        for (i, &value) in original.iter().enumerate() {
+            ans[i / n as usize][i % n as usize] = value;
         }
 
         ans
