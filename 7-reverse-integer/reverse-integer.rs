c014ba8f1cde863184a82db4e67712675c9fd887
Hruday <150934405+hruday-HMS69@users.noreply.github.com>
2024-09-11 22:38:13 +0530

    Time: 3 ms (35.88%) | Memory: 2 MB (81.76%) - LeetSync

--- 7-reverse-integer/reverse-integer.rs
+++ 7-reverse-integer/reverse-integer.rs
@@ -1,21 +1,16 @@
 impl Solution {
-    pub fn reverse(x: i32) -> i32 {
-        let mut c_value = x;
-        let mut sum = 0;
-        let mut digit = c_value % 10;
-        c_value /= 10;
-        while c_value != 0 {
-            sum *= 10;
-            sum += digit;
-            digit = c_value % 10;
-            c_value /= 10;
-            println!("{} {} \t\t sum {}", c_value, digit, sum);
-        }
-        if sum >  214748364 || sum ==  214748364 && digit >  7 
-        || sum < -214748364 || sum == -214748364 && digit < -8 {
-            0
-        } else {
-            sum * 10 + digit
+    pub fn reverse(mut x: i32) -> i32 {
+        let is_minus = x < 0;
+        match x
+            .abs()
+            .to_string()
+            .chars()
+            .rev()
+            .collect::<String>()
+            .parse::<i32>()
+        {
+            Ok(x) => x * (if is_minus { -1 } else { 1 }),
+            Err(_) => 0,
         }
     }
 }
