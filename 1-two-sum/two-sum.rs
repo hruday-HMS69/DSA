07bda32df5cc029ec71d775e7af02fba7f8c5145
Hruday <150934405+hruday-HMS69@users.noreply.github.com>
2024-09-13 23:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 2.5 MB (8.87%) - LeetSync

--- 1-two-sum/two-sum.rs
+++ 1-two-sum/two-sum.rs
@@ -1,11 +1,13 @@
+use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for i in 0..nums.len() {
-            for j in i+1..nums.len() {
-                if nums[i] + nums[j] == target {
-                    return vec![i as i32, j as i32];
-                }
+        let mut map = HashMap::new();
+        for (i, &value) in nums.iter().enumerate() {
+            let complement = target - value;
+            if map.contains_key(&complement) {
+                return vec![*map.get(&complement).unwrap() as i32, i as i32];
             }
+            map.insert(value, i);
         }
         vec![]
     }
