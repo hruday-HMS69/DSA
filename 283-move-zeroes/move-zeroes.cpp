89c984f81e7835009d2484a32626a83d40488a8a
Hruday <150934405+hruday-HMS69@users.noreply.github.com>
2025-03-31 15:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (17.66%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& a) {  
-        int n = a.size();  
-        vector<int> temp;
-        for (int i = 0; i < n; i++) {
-            if (a[i] != 0) {
-                temp.push_back(a[i]);
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();  
+        int j = -1; 
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 0) {
+                j = i;
+                break;
             }
         }
-        int nz = temp.size();
-        for (int i = 0; i < nz; i++) {
-            a[i] = temp[i];
-        }
-        for (int i = nz; i < n; i++) {
-            a[i] = 0;
+
+        if(j == -1) return;
+
+        for(int i = j + 1; i < n; i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;  
+            }
         }
     }
 };
