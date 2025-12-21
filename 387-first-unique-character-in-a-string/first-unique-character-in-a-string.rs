62199fed9782fc132f6c14b7f7bd8c8f5e99fa10
Hruday <150934405+hruday-HMS69@users.noreply.github.com>
2025-12-21 20:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 2.4 MB (36.23%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
@@ -1,13 +1,15 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn first_uniq_char(s: String) -> i32 {
-        let mut freq: HashMap<char,i32> = HashMap::new();
+        let mut freq= [0; 26];
         
         for ch in s.chars(){
-            *freq.entry(ch).or_insert(0) += 1;
+            let idx = (ch as u8 - b'a') as usize;
+            freq[idx] += 1;
         }
         for (i,ch) in s.chars().enumerate(){
-            if freq[&ch]==1{
+            let idx = (ch as u8 - b'a') as usize;
+            if freq[idx] == 1 {
                 return i as i32;
             }
         }
