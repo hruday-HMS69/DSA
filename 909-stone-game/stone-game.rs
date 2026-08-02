2d8d8a6c80b58f5141e8233486e656de7c605717
Hruday <150934405+hruday-HMS69@users.noreply.github.com>
2026-08-02 22:20:59 +0530

    Time: 0 ms (100.00%) | Memory: 3 MB (20.00%) - LeetSync

---
+++ 909-stone-game/stone-game.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn stone_game(piles: Vec<i32>) -> bool {
+        let n = piles.len();
+        let mut dp = vec![vec![0; n]; n];
+
+        for i in 0..n {
+            dp[i][i] = piles[i];
+        }
+
+        for len in 2..=n {
+            for i in 0..=n - len {
+                let j = i + len - 1;
+                dp[i][j] = std::cmp::max(
+                    piles[i] - dp[i + 1][j],
+                    piles[j] - dp[i][j - 1],
+                );
+            }
+        }
+
+        dp[0][n - 1] > 0
+    }
+}
